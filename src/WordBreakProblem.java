df29eb148da3d7a9164f6ffdd2daa816f951a27a
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-05 12:40:00 +0530

    Tries

--- src/WordBreakProblem.java
+++ src/WordBreakProblem.java
@@ -5,6 +5,54 @@
 
 public class WordBreakProblem {
 
+    public static class Node {
+        Node[] children = new Node[26];
+        boolean endOfWord = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node(); // root always remain empty
+
+    public static void insert(String word) {    // O(L) L = Largest word length
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.endOfWord = true;
+    }
+
+    public static boolean search(String key) {   // Time Complexity = O(L)
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return curr.endOfWord == true;
+    }
+
+    public static boolean wordBreak(String key) {
+        for (int i = 1; i < key.length(); i++) {
+            if (search(key.substring(0, i)) && wordBreak(key.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
     }
