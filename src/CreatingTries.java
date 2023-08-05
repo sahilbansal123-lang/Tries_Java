d7af4c695b5d8ba7911b0611e39a12c8383192d9
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-05 11:38:03 +0530

    Tries

--- src/CreatingTries.java
+++ src/CreatingTries.java
@@ -1,5 +1,3 @@
-import java.security.PublicKey;
-
 public class CreatingTries {
 
     public static class Node {
@@ -15,11 +13,20 @@ public class CreatingTries {
 
     public static Node root = new Node(); // root always remain empty
 
-    public static void insert(String str) {
+    public static void insert(String word) {    // O(L) L = Largest word length
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
 
+        curr.endOfWord = true;
     }
 
     public static void main(String[] args) {
-        String word[] = {"the", "a", "there", "their", "any", "thee"};
+        String[] word = {"the", "a", "there", "their", "any", "thee"};
     }
 }
