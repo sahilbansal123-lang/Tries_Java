ab833464417f9d595e58bdc62ed92df6677689db
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-05 23:28:55 +0530

    Tries

--- src/PrefixProblem.java
+++ src/PrefixProblem.java
@@ -3,15 +3,17 @@ public class PrefixProblem {
     public static class Node {
         Node [] children = new Node[26];
         boolean endOfWord = false;
-        int freq = 0;
+        int freq;
 
         Node() {
             for (int i = 0; i < 26; i++) {
                 children[i] = null;
             }
+            freq = 1;
         }
     }
 
+    public static Node root = new Node();
 
     public static void main(String[] args) {
 
