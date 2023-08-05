4b68b03990973af4c665fb89c211641cff93363a
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-05 12:13:38 +0530

    Tries

--- src/WordBreakProblem.java
+++ src/WordBreakProblem.java
@@ -1,3 +1,8 @@
+
+// Given an input string and a dictionary of words, find out if the input string can be segmented into a space-separated
+// sequence of dictionary words. See following examples for more details.
+// This is a famous Google interview question, also being asked by many other companies now a days.
+
 public class WordBreakProblem {
 
     public static void main(String[] args) {
