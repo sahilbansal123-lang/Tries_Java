7a05695d999712f07fc5b484b2961233bbb8bc35
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-06 22:54:52 +0530

    Tries

--- src/StartsWithProblem.java
+++ src/StartsWithProblem.java
@@ -1,5 +1,11 @@
 public class StartsWithProblem {
 
+    //  Create a function boolean startWith for a trie
+    //  returns true if a word start with given prefix else returns false
+
+    public static boolean startsWith(String word, String prefix) {
+
+    }
 
     public static void main(String[] args) {
 
